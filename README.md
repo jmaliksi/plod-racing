ec35f1fc2aee945100d2ad4088b5d147678d2244
joe <joe.maliksi@gmail.com>
2021-08-02 00:26:14 +0200

    snail

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # plod-racing
 Now this is plod racing
+
+icon from https://game-icons.net/1x1/lorc/snail.html
--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-//import { store } from './app/store';
+import store from './app/store';
 import { Provider } from 'react-redux';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={}>
+    <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>,
